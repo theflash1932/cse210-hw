82d74860bb4a71ce1398a45e12a06ef850f7e8fe
theflash1932 <greg@gregnielson.com>
2025-09-08 05:57:06 -0600

    Finished assignment 2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -1,9 +1,54 @@
 using System;
+using System.Runtime.CompilerServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("What is your grade percentage? ");
+        string gradeInput = Console.ReadLine();
+        int grade = int.Parse(gradeInput);
+        string letterGrade;
+        string qualifier = "";
+
+        // Determine letter grade based on percentage
+        if (grade >= 90)
+        {
+            letterGrade = "A";
+        }
+        else if (grade >= 80)
+        {
+            letterGrade = "B";
+        }
+        else if (grade >= 70)
+        {
+            letterGrade = "C";
+        }
+        else if (grade >= 60)
+        {
+            letterGrade = "D";
+        }
+        else letterGrade = "F";
+
+        // Determine + / - for grade qualifier
+        if (grade >= 60 && grade < 97)
+        {
+            if (grade % 10 >= 7)
+            {
+                qualifier = "+";
+            }
+            if (grade % 10 < 3)
+            {
+                qualifier = "-";
+            }
+        }
+        Console.WriteLine($"Your letter grade: {letterGrade}{qualifier}");
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congratulations, you passed!");
+        }
+        else
+            Console.WriteLine("Sorry, you didn't pass. Better luck next time!");
+
     }
 }
