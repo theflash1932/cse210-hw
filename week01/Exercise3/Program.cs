99da856ed1f7181df7d6bd73f94bdc8a1c6abccc
theflash1932 <greg@gregnielson.com>
2025-09-08 06:32:32 -0600

    Finished assignment 3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,9 +1,38 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        string playAgain = "";
+        do
+        {
+            Random randomGenerator = new Random();
+            int magicNumber = randomGenerator.Next(1, 11);
+            int number;
+            int guesses = 0;
+            string qty = "try";
+            do
+            {
+                Console.Write("What is the magic number? ");
+                number = int.Parse(Console.ReadLine());
+                if (magicNumber > number)
+                {
+                    Console.WriteLine("Higher");
+                }
+                if (magicNumber < number)
+                {
+                    Console.WriteLine("Lower");
+                }
+                guesses++;
+            }
+            while (number != magicNumber);
+            if (guesses > 1) qty = "tries";
+            Console.WriteLine($"You got it in {guesses} {qty}!!");
+            Console.Write("Would you like to play again? N to stop: ");
+            playAgain = Console.ReadLine();
+        }
+        while (!playAgain.ToLower().Contains('n'));
     }
 }
