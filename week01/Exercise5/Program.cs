bf8299e3bc0302e6471e5a3e7acba28bbd9f1591
theflash1932 <greg@gregnielson.com>
2025-09-09 05:57:51 -0600

    Assignment 5

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        DisplayWelcome();
+        DisplayResult(PromptUserName(),PromptUserNumber());
+    }
+
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the Program!");
+    }
+    static String PromptUserName()
+    {
+        Console.Write("What is your name? ");
+        return Console.ReadLine();
+    }
+    static int PromptUserNumber()
+    {
+        Console.Write("What is your favorite number? ");
+        return int.Parse(Console.ReadLine());
+    }
+    static int SquareNumber(int i) {
+        return i * i;
+    }
+    static void DisplayResult(String u, int i)
+    {
+        Console.WriteLine($"Hi {u}! The square of your favorite number is {SquareNumber(i)}");
     }
 }
