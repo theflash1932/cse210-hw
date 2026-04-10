2181f0913ce36c5c41ddf46cac8cddf185f61c57
Greg Nielson <gnielson@techlegion.com>
2026-04-09 22:53:10 -0600

    now

--- week05/Mindfulness/Program.cs
+++ week05/Mindfulness/Program.cs
@@ -5,5 +5,10 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Mindfulness Project.");
+        Console.WriteLine("Menu\n----");
+        Console.WriteLine("");
+        Console.WriteLine("");
+        Console.WriteLine("");
+        
     }
 }
