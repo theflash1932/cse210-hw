3f0a7913299c46dc25923ce0e4f9326a71b69bc0
Greg Nielson <greg@gregnielson.com>
2026-05-18 20:27:27 -0600

    week 3 exc

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -14,13 +14,13 @@ class Program
         Fraction f2 = new Fraction(5);
         Console.WriteLine("One parameter: " + f2.GetFractionString());
         Console.WriteLine("One parameter: " + f2.GetDecimalValue());
-        Fraction f3 = new Fraction(3,4);
+        Fraction f3 = new Fraction(3, 4);
         Console.WriteLine("Two parameter: " + f3.GetFractionString());
         Console.WriteLine("Two parameter: " + f3.GetDecimalValue());
-        Fraction f4 = new Fraction(1,3);
+        Fraction f4 = new Fraction(1, 3);
         Console.WriteLine("Two parameter: " + f4.GetFractionString());
         Console.WriteLine("Two parameter: " + f4.GetDecimalValue());
-        //test output formats
+        //test output formats 
     }
 
 }
