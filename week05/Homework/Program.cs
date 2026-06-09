d5583a8cefa07085c56f8b712cfc92e8ff4624b1
Greg Nielson <greg@gregnielson.com>
2026-06-09 09:32:51 -0600

    d

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -1,4 +1,4 @@
-using System; 
+using System;
 
 namespace Homework
 {
@@ -7,20 +7,20 @@ namespace Homework
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World! This is the Homework Project.");
-            Assignment a = new Assignment("Samuel Bennett","Multiplication");
-            MathAssignment b = new MathAssignment("Roberto Rodriquez","Fractions","Section 7.3","Problems 8-19");
-            WritingAssignment c = new WritingAssignment("Mary Waters","European History","The Causes of World War II by Mary Waters");
-        
-            Console.WriteLine();        
+            Assignment a = new Assignment("Samuel Bennett", "Multiplication");
+            MathAssignment b = new MathAssignment("Roberto Rodriquez", "Fractions", "Section 7.3", "Problems 8-19");
+            WritingAssignment c = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II by Mary Waters");
+
+            Console.WriteLine();
             Console.WriteLine(a.GetSummary());
-            Console.WriteLine();        
+            Console.WriteLine();
             Console.WriteLine(b.GetSummary());
             Console.WriteLine(b.GetHomeworkList());
-            Console.WriteLine();        
+            Console.WriteLine();
             Console.WriteLine(c.GetSummary());
             Console.WriteLine(c.GetWritingInformation());
-            Console.WriteLine();        
-            
+            Console.WriteLine();
+
         }
     }
 }
